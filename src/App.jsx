7ce1b31a2cb0c79bb8c13d91eae83ec190e4f4cf
VishalDipake <vishaldipake072@gmail.com>
2026-03-20 20:13:56 +0530

    feat: educator dashboard complete

--- src/App.jsx
+++ src/App.jsx
@@ -32,12 +32,12 @@ const App = () => {
          <Route path='/player/:courseId' element={<Player />} />
           <Route path='/loading/:path' element={<Loading />} />
 
-          <Route path ='/educator' element={<Educator />}> 
-              <Route path='educator' element={<Dashboard/>}/>
-               <Route path='add-course' element={<AddCourse/>}/>
-                <Route path='my-courses' element={<MyCourses/>}/>
-                 <Route path='student-enrolled' element={<StudentsEnrolled/>}/>
-          </Route>
+         <Route path='/educator' element={<Educator />}>
+    <Route index element={<Dashboard />} />   // 🔥 THIS IS IMPORTANT
+    <Route path='add-course' element={<AddCourse />} />
+    <Route path='my-courses' element={<MyCourses />} />
+    <Route path='student-enrolled' element={<StudentsEnrolled />} />
+      </Route>
       </Routes>
     </div>
   )
